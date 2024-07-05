9e363d5b5cba59ce5db68afc6e94350cd8c63e5c
Jan Kučera <xkucera4@fi.muni.cz>
2024-07-05 18:09:21 +0200

    Add spring boot template

---
+++ src/main/java/com/example/inqooltennisreservationapi/InQoolTennisReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.inqooltennisreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InQoolTennisReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InQoolTennisReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inqooltennisreservationapi/InQoolTennisReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inqooltennisreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InQoolTennisReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
