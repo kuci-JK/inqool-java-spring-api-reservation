376dee6e745e930f0dc2c8f0951f91aa769ce3c9
Jan Kučera <xkucera4@fi.muni.cz>
2024-07-06 00:01:03 +0200

    feat: add Reservation model

---
+++ src/main/java/com/example/inqooltennisreservationapi/model/Reservation.java
@@ -0,0 +1,46 @@
+package com.example.inqooltennisreservationapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Entity
+@Table(name = "reservations")
+public class Reservation extends SoftDeletableEntity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "created_date", nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "reservation_start", nullable = false)
+    private LocalDateTime reservationStart;
+
+    @Column(name = "reservation_end", nullable = false)
+    // TODO validate reservationStart < reservationEnd...
+    private LocalDateTime reservationEnd;
+
+    @Column(name = "game_type", nullable = false)
+    private GameType gameType;
+
+    @Column(name = "total_price", nullable = false)
+    private double totalPrice;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "court_id", nullable = false)
+    private Court reservedCourt;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
