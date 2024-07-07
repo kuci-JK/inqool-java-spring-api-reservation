d9b7b426f06af848464f6b9ac2256c261d912f7a
Jan Kučera <xkucera4@fi.muni.cz>
2024-07-07 22:53:31 +0200

    fix: common surface api prefix

--- src/main/java/com/example/inqooltennisreservationapi/controller/SurfaceController.java
+++ src/main/java/com/example/inqooltennisreservationapi/controller/SurfaceController.java
@@ -4,30 +4,31 @@ import com.example.inqooltennisreservationapi.model.CourtSurface;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/surfaces")
 public class SurfaceController {
 
-    @GetMapping("/surfaces")
+    @GetMapping("/")
     public String listSurfaces() {
         // TODO list all surfaces
         return "TODO list surfaces";
     }
 
-    @PostMapping("/surfaces")
+    @PostMapping("/")
     public String createSurface(@RequestBody CourtSurface surface) {
         return "TODO create surface: " + surface;
     }
 
-    @GetMapping("/surfaces/{surfaceId}" )
+    @GetMapping("/{surfaceId}" )
     public String getSurface(@PathVariable Long surfaceId) {
         return "TODO get surface " + surfaceId;
     }
 
-    @PutMapping("/surfaces/{surfaceId}" )
+    @PutMapping("/{surfaceId}" )
     public String updateSurface(@PathVariable Long surfaceId, @RequestBody CourtSurface data) {
         return "TODO update surface: '" + surfaceId + "' - data: " + data;
     }
 
-    @DeleteMapping("/surfaces/{surfaceId}")
+    @DeleteMapping("/{surfaceId}")
     public String deleteSurface(@PathVariable Long surfaceId) {
         return "TODO delete surface: '" + surfaceId + "'";
     }
