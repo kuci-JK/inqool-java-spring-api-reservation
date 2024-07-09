c381f196a59b30b7c6dc77aa19b9ecefcf6c98c6
Jan Kučera <xkucera4@fi.muni.cz>
2024-07-09 23:25:38 +0200

    feat: user repository interface

---
+++ src/main/java/com/example/inqooltennisreservationapi/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.inqooltennisreservationapi.repository;
+
+import com.example.inqooltennisreservationapi.model.entity.UserEntity;
+
+import java.util.Optional;
+
+public interface UserRepository {
+
+    Optional<UserEntity> createUser(UserEntity userEntity);
+
+    Optional<UserEntity> updateUser(long id, UserEntity updatedUserEntity);
+
+    Optional<UserEntity> deleteUser(long id);
+
+    Optional<UserEntity> getUserById(long id);
+
+}
